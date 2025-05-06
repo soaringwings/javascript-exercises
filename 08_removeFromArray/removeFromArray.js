bf840bd52861851e26abb8e0ab96d1727b3d7828
soaringwings <135184390+soaringwings@users.noreply.github.com>
2025-05-06 10:11:16 -0400

    Complete Exercises 1-7

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+    for (const removedValue of removedValues) {
+        array = array.filter(function(value) {
+            return value !== removedValue;
+        })
+    }
+    return array
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let smaller;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR"
+    } else if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
+        return "ERROR";
+    } 
+    if (num1 < num2) {
+        smaller = num1;
+        larger = num2
+    } else {
+        smaller = num2;
+        larger = num1;
+    }
+    let sum = smaller;
+    for (let i = smaller + 1; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

bf6fd671e3cf782cea13c0fad9acfd78d6bc45c6
soaringwings <135184390+soaringwings@users.noreply.github.com>
2025-05-06 10:11:16 -0400

    Complete Exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = "";
+    for (let i = 1; i <= string.length; i++) {
+        reversed += string.at(-i)
+    }
+    return reversed
 
 };
 
